e0ebc7c3eb6679194908925bf53a97810a2d8bae
DarkTheme644 <anastasijakovtun32@gmail.com>
2024-10-11 17:47:39 +0300

    Добавил несолько вкладок

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from .config import Config
+from config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
